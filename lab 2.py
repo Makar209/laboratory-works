f4f138bd1470f2682b9be9275fb27f3fd6d59b09
devilsschool <devilsschool@mail.ru>
2022-11-05 20:05:08 +0300

    Вывел скорость ветра для текущего прогноза и для прогноза на неделюlab 2.py

--- lab 2.py
+++ lab 2.py
@@ -6,6 +6,7 @@ resOnToDay = requests.get("http://api.openweathermap.org/data/2.5/weather",
     params={'q': city, 'units': 'metric', 'lang': 'ru','APPID': appid,})
 dataOnToday = resOnToDay.json()
 #Прогноз погоды на день
+print("Прогноз погоды на день")
 print("Город:", city)
 print("Погодные условия:", dataOnToday['weather'][0]['description'])
 print("Температура:", dataOnToday['main']['temp'])
@@ -18,10 +19,24 @@ resOnWeek = requests.get("http://api.openweathermap.org/data/2.5/forecast",
     params={'q': city, 'units': 'metric', 'lang': 'ru','APPID': appid,})
 dataOnWeek = resOnWeek.json()
 
-#Прогноз работы на неделю
+#Прогноз погоды на неделю
 print("Прогноз погоды на неделю:")
 for i in dataOnWeek['list']:
     print("Дата <", i['dt_txt'], "> \r\nТемпература <",
 '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <",
 i['weather'][0]['description'], ">")
 print("____________________________")
+
+#Задание для самостоятельного решения:
+#Вывести скорость ветра и видимость для текущего прогноза и для прогноза на неделю
+
+print("Прогноз ветра и видимости на сегодня:")
+print('Cкорость ветра: ', dataOnToday['wind']['speed'], ' м/с')
+print("Видимость: ", (dataOnToday['visibility'] / 10000)*100, '%')
+print('----------------------------------')
+
+print("Прогноз ветра и видимости на неделю:")
+for i in dataOnWeek["list"]:
+    print("Дата: ", i['dt_txt'], 
+    "\r\n Скорость ветра: ", i['wind']['speed'], ' м/с',
+    "\r\n Видимость: ", round((i['visibility'] / 10000)*100), '%')
