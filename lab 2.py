8087f3abd4bd8b4d66cce38d66e262bc9c40edb9
devilsschool <devilsschool@mail.ru>
2022-11-05 19:21:36 +0300

    Сделал запрос недельной погоды и вывел её

--- lab 2.py
+++ lab 2.py
@@ -2,12 +2,26 @@ import requests
 
 city = "Moscow, RU"
 appid = "fd10e3819f04b731074a39f329b73d3b"
-res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
-data = res.json()
-
+resOnToDay = requests.get("http://api.openweathermap.org/data/2.5/weather",
+    params={'q': city, 'units': 'metric', 'lang': 'ru','APPID': appid,})
+dataOnToday = resOnToDay.json()
+#Прогноз погоды на день
 print("Город:", city)
-print("Погодные условия:", data['weather'][0]['description'])
-print("Температура:", data['main']['temp'])
-print("Минимальная температура:", data['main']['temp_min'])
-print("Максимальная температура", data['main']['temp_max'])
+print("Погодные условия:", dataOnToday['weather'][0]['description'])
+print("Температура:", dataOnToday['main']['temp'])
+print("Минимальная температура:", dataOnToday['main']['temp_min'])
+print("Максимальная температура", dataOnToday['main']['temp_max'])
+print("____________________________")
+
+
+resOnWeek = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+    params={'q': city, 'units': 'metric', 'lang': 'ru','APPID': appid,})
+dataOnWeek = resOnWeek.json()
+
+#Прогноз работы на неделю
+print("Прогноз погоды на неделю:")
+for i in dataOnWeek['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <",
+'{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <",
+i['weather'][0]['description'], ">")
+print("____________________________")
