15671d5eec88207b43e35d4146c8867db8c3ec9c
devilsschool <devilsschool@mail.ru>
2022-11-05 18:44:29 +0300

    Подключил библиотеку, отправил запрос, вывел резы

---
+++ lab 2.py
@@ -0,0 +1,13 @@
+import requests
+
+city = "Moscow, RU"
+appid = "fd10e3819f04b731074a39f329b73d3b"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+
+print("Город:", city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
